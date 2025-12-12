610286a318f40178708acb71ef0130ea8fcc92c1
undertale562-collab <undertale562@gmail.com>
2025-12-12 07:53:51 +0300

    Update painting.cpp

--- src/painting.cpp
+++ src/painting.cpp
@@ -1,7 +1,14 @@
 #include "painting.h"
+#include <QHBoxLayout>
 
 Painting::Painting(){
-
+    mousePressed=false;
+    // QHBoxLayout* base=new QHBoxLayout;
+    // desk=new QLabel;
+    // canvas.scaled(widgetSize);
+    // desk->setPixmap(canvas);
+    // base->addWidget(desk);
+    // this->setLayout(base);
 }
 
 Painting::~Painting(){
@@ -9,9 +16,8 @@ Painting::~Painting(){
 }
 
 void Painting::mouseMoveEvent(QMouseEvent* event){
-    if(mousePressed){
-        position=event->pos();
-    }
+    position=event->pos();
+    update();
 }
 
 void Painting::mousePressEvent(QMouseEvent*){
@@ -24,11 +30,58 @@ void Painting::mouseReleaseEvent(QMouseEvent*){
 }
 
 void Painting::paintEvent(QPaintEvent*){
-    QPainter painter(this);
-    QBrush brush(Qt::SolidPattern);
-    brush.setColor(streak.strokeColor);
-    painter.setPen(Qt::NoPen);
-    painter.setBrush(brush);
-    painter.drawRect(300, 200, streak.strokeSize.width(), streak.strokeSize.height());
+    if(mousePressed){
+        painter=new QPainter(this);
+        QBrush brush(Qt::SolidPattern);
+        brush.setColor(streak.strokeColor);
+        painter->setPen(Qt::NoPen);
+        painter->setBrush(brush);
+        if(brushShape=="rect"){
+            painter->drawRect(position.x(), position.y(), streak.strokeSize.width(), streak.strokeSize.height());
+        }
+        else if(brushShape=="circ"){
+            painter->drawEllipse(position.x(), position.y(), streak.strokeSize.width(), streak.strokeSize.height());
+        }
+        painter->end();
+    }
+}
+
+void Painting::BrushChanged(QString modifier){
+    if(modifier=="RectB"){
+        brushShape="rect";
+    }
+    else if(modifier=="CircB"){
+        brushShape="circ";
+    }
+}
+
+void Painting::ShapeChanged(QString modifier){
+    if(modifier=="Rect"){
+        figure="rect";
+    }
+    else if(modifier=="Circ"){
+        figure="circ";
+    }
+}
+
+void Painting::ColorChanged(QString modifier){
+    if(modifier=="Red"){
+        streak.strokeColor={255,0,0};
+    }
+    else if(modifier=="Blue"){
+        streak.strokeColor={0,0,255};
+    }
+    else if(modifier=="Green"){
+        streak.strokeColor={0,255,0};
+    }
+    else if(modifier=="Yellow"){
+        streak.strokeColor={255,255,0};
+    }
+    else if(modifier=="Black"){
+        streak.strokeColor={0,0,0};
+    }
+    else if(modifier=="White"){
+        streak.strokeColor={255,255,255};
+    }
 }
 
