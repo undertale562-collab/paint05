800630a918a316ff24e9cc8eb25da3b44bfb603f
Daimond <undertale562@gmail.com>
2025-12-15 20:52:40 +0300

    Update painting.cpp

--- src/painting.cpp
+++ src/painting.cpp
@@ -10,12 +10,27 @@ Painting::~Painting(){
 
 }
 
-// вот здесь то, что писал я
-
-
 void Painting::mouseMoveEvent(QMouseEvent* event){
-    position=event->pos();
-    update();
+    if(streak.erase){
+        for(int i=0; i<massive.size(); i++){
+            if(event->pos().x()+streak.strokeSize.width()/2 > massive[i].thisonePosition.x() &&
+                event->pos().x()+streak.strokeSize.width()/2 < massive[i].thisonePosition.x()+massive[i].size.width() &&
+                event->pos().y()+streak.strokeSize.height()/2 > massive[i].thisonePosition.y() &&
+                event->pos().y()+streak.strokeSize.height()/2 < massive[i].thisonePosition.y()+massive[i].size.height()){
+                massive.erase(massive.begin()+i);
+                update();
+            }
+        }
+    }
+    else{
+        Thisone piece;
+        piece.thisonePosition=event->pos();
+        piece.color=streak.strokeColor;
+        piece.size=streak.strokeSize;
+        piece.shapeB=brushShape;
+        massive.push_back(piece);
+        update();
+    }
 }
 
 void Painting::mousePressEvent(QMouseEvent* event){
@@ -29,34 +44,19 @@ void Painting::mouseReleaseEvent(QMouseEvent*){
 
 void Painting::paintEvent(QPaintEvent*){
     painter=new QPainter(this);
-    if(mousePressed){
-        QBrush brush;
-        brush.setColor(streak.strokeColor);
-
-        if(figure==""){
-            brush.setStyle(Qt::SolidPattern);
-        }
-        else{
-            brush.setStyle(Qt::NoBrush);
-            painter->setPen(Qt::SolidLine);
-
-            if(figure=="rect"){
-            }
-            else{
-
-            }
-            painter->end();
-        }
-
-        painter->setPen(Qt::NoPen);
+    painter->setPen(Qt::NoPen);
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    for (int i=0; i<massive.size(); i++){
+        brush.setColor(massive[i].color);
         painter->setBrush(brush);
-        if(brushShape=="rect"){
-            painter->drawRect(position.x()-streak.strokeSize.width()/2, position.y()-streak.strokeSize.height()/2,
-                              streak.strokeSize.width(), streak.strokeSize.height());
+        if(massive[i].shapeB=="rect"){
+            painter->drawRect(massive[i].thisonePosition.x()-massive[i].size.width()/2,
+                              massive[i].thisonePosition.y()-massive[i].size.height()/2, massive[i].size.width(), massive[i].size.height());
         }
-        else if(brushShape=="circ"){
-            painter->drawEllipse(position.x()-streak.strokeSize.width()/2, position.y()-streak.strokeSize.height()/2,
-                                 streak.strokeSize.width(), streak.strokeSize.height());
+        else if(massive[i].shapeB=="circ"){
+            painter->drawEllipse(massive[i].thisonePosition.x()-massive[i].size.width()/2,
+                                 massive[i].thisonePosition.y()-massive[i].size.height()/2, massive[i].size.width(), massive[i].size.height());
         }
     }
     painter->end();
@@ -104,68 +104,43 @@ void Painting::ColorChanged(QString modifier){
     }
 }
 
+void Painting::SizeChanged(QString modifier){
+    if(modifier=="Thicker"){
+        if(streak.strokeSize.width()==1){
+            streak.strokeSize={10, streak.strokeSize.height()};
+        }
+        else{
+            streak.strokeSize={streak.strokeSize.width()+10, streak.strokeSize.height()};
+        }
+    }
+    else if(modifier=="Thinner"){
+        streak.strokeSize={streak.strokeSize.width()-10, streak.strokeSize.height()};
+        if(streak.strokeSize.width()==0){
+            streak.strokeSize={1, streak.strokeSize.height()};
+        }
+    }
+    if(modifier=="Higher"){
+        if(streak.strokeSize.width()==1){
+            streak.strokeSize={streak.strokeSize.width(), 1};
+        }
+        else{
+            streak.strokeSize={streak.strokeSize.width(), streak.strokeSize.height()+10};
+        }
+    }
+    else if(modifier=="Lower"){
+        streak.strokeSize={streak.strokeSize.width(), streak.strokeSize.height()-10};
+        if(streak.strokeSize.height()==0){
+            streak.strokeSize={streak.strokeSize.height(), 1};
+        }
+    }
+}
 
-// void Painting::mousePressEvent(QMouseEvent *event)
-// {
-//     if (event->button() == Qt::LeftButton) {
-//         position = event->pos();
-//         mousePressed = true;
-//     }
-// }
-
-// // When the mouse moves if the left button is clicked
-// // we call the drawline function which draws a line
-// // from the last position to the current
-// void Painting::mouseMoveEvent(QMouseEvent *event)
-// {
-//     if ((event->buttons() & Qt::LeftButton) && mousePressed)
-//         drawLineTo(event->pos());
-// }
-
-// // If the button is released we set variables to stop drawing
-// void Painting::mouseReleaseEvent(QMouseEvent *event)
-// {
-//     if (event->button() == Qt::LeftButton && mousePressed) {
-//         drawLineTo(event->pos());
-//         mousePressed = false;
-//     }
-// }
-
-// // QPainter provides functions to draw on the widget
-// // The QPaintEvent is sent to widgets that need to
-// // update themselves
-// void Painting::paintEvent(QPaintEvent *event)
-// {
-//     QPainter painter(this);
-
-//     // Returns the rectangle that needs to be updated
-//     QRect dirtyRect = event->rect();
-
-//     // Draws the rectangle where the image needs to
-//     // be updated
-//     painter.drawImage(dirtyRect, image, dirtyRect);
-// }
-
-// void Painting::drawLineTo(const QPoint &endPoint){
-//     // Used to draw on the widget
-//     QPainter painter(&image);
-
-//     // Set the current settings for the pen
-//     painter.setPen(QPen(streak.strokeColor, 30, Qt::SolidLine, Qt::RoundCap,
-//                         Qt::RoundJoin));
-
-//     // Draw a line from the last registered point to the current
-//     painter.drawLine(position, endPoint);
-
-//     int rad = (30 / 2) + 2;
-
-//     // Call to update the rectangular space where we drew
-//     update(QRect(position, endPoint).normalized()
-//                .adjusted(-rad, -rad, +rad, +rad));
-
-//     // Update the last position where we left off drawing
-//     position = endPoint;
-// }
-
-
-
+void Painting::Desintegrate(QString modifier){
+    if(modifier=="Clear"){
+        massive={};
+        update();
+    }
+    else if(modifier=="Eraser"){
+        streak.erase=true;
+    }
+}
