501cbcf8f1f0e75e05d6a6f6fa3bfd2047528a35
Daimond <undertale562@gmail.com>
2025-12-15 20:53:19 +0300

    Update painting.h

--- src/painting.h
+++ src/painting.h
@@ -1,13 +1,21 @@
 #include <QPainter>
 #include <QMouseEvent>
 #include <QWidget>
-#include <QImage>
+#include <vector>
 
 #pragma once
 
 struct Stroke{
-    QSize strokeSize{50,50};
+    QSize strokeSize{20,20};
     QColor strokeColor{0, 191, 255};
+    bool erase=false;
+};
+
+struct Thisone{
+    QString shapeB;
+    QColor color;
+    QSize size;
+    QPoint thisonePosition;
 };
 
 class Painting: public QWidget{
@@ -20,19 +28,17 @@ protected:
     void mouseMoveEvent(QMouseEvent*) override;
     void mousePressEvent(QMouseEvent*) override;
     void mouseReleaseEvent(QMouseEvent*) override;
-    // void drawLineTo(const QPoint& endPoint);
 private:
     bool mousePressed;
     QString brushShape="rect";
     QString figure="";
-    QPoint position={0,0};
     Stroke streak;
     QPainter* painter;
-    // QImage image;
+    std::vector<Thisone> massive={};
 public slots:
     void BrushChanged(QString);
     void ShapeChanged(QString);
     void ColorChanged(QString);
+    void SizeChanged(QString);
+    void Desintegrate(QString);
 };
-
-
